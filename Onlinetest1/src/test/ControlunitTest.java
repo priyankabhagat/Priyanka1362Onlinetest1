029a0a654c1233c97e17cf87847a0db92e8de450
priyankabhagat <priyanka304b@gmail.com>
2015-07-09 17:26:56 +0530

    EVM

---
+++ Onlinetest1/src/test/ControlunitTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControlunitTest {
+
+	@Test
+	public void testOnlamp() {
+		//fail("Not yet implemented");
+		assertEquals(true,true);
+		System.out.println("the lamp is on");
+	}
+
+}
